85efb10d972898c52c521354b9e4b83fcc5e349d
Anas <sm.anas@outlook.com>
2020-10-27 17:37:15 +0530

    Implement change request
    Fixed changes requested in pull-request
     - Divided the bubble_sort and bubble_sort_by into two different files

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,9 +1,10 @@
-def bubble_sort(array)
+def bubble_sort_by(array)
   n = array.length
   loop do
     switched = false
     (n - 1).times do |x|
-      if array[x] > array[x + 1]
+      result = yield array[x], array[x + 1]
+      if result.positive?
         array[x], array[x + 1] = array[x + 1], array[x]
         switched = true
       end
@@ -14,4 +15,4 @@ def bubble_sort(array)
   array
 end
 
-p bubble_sort([9, 23, 8, 11, 6, 9, 4, 3, 1])
+p bubble_sort_by(%w[Longest Misori Anas Lameck Binyam Cy Sinan]) { |left, right| left.length - right.length }
