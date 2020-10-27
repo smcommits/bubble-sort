48d8b72f44b1c2e7ae578b96de9196a9ee7bf3f1
Misori-simon <misorisimon@gmail.com>
2020-10-27 10:36:31 +0100

    Implemented the bubble sort by method -initialised the input array length -set the switched variable to false -performed block consitions on yielded variables

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+# rubocop: disable Metrics/MethodLength
+
 def bubble_sort(array)
   n = array.length
   loop do
@@ -15,3 +17,24 @@ def bubble_sort(array)
   end
   array
 end
+
+p bubble_sort([9, 23, 8, 11, 6, 9, 4, 3, 1])
+
+def bubble_sort_by(array)
+  n = array.length
+  loop do
+    switched = false
+    (n - 1).times do |x|
+      result = yield array[x], array[x + 1]
+      if result.positive?
+        array[x], array[x + 1] = array[x + 1], array[x]
+        switched = true
+      end
+    end
+    n -= 1
+    break unless switched == true
+  end
+  array
+end
+
+p bubble_sort_by(%w[Longest Misori Anas Lameck Binyam Cy Sinan]) { |left, right| left.length - right.length }
